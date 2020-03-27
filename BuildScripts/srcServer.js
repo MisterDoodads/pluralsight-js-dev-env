7ffbf80dc4caa8fb457fe850040191077070b4da
Gaston Ayoroa <gda1701@hotmail.com>
2020-03-27 14:27:06 -0400

    Adding a lot of progress with the course.

--- BuildScripts/srcServer.js
+++ BuildScripts/srcServer.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from '../node_modules/express';
+import path from 'path';
+import open from '../node_modules/open';
+import webpack from '../node_modules/webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('../node_modules/webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 // Set up the routing
 // At the root get.
